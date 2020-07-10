bf15c0b366c049f1f71117e5ab9d0b86404e9d0c
GregoryConrad <gregorysconrad@gmail.com>
2020-07-10 19:37:57 -0400

    Extracted functionality into base class

---
+++ lib/simulation_base.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:ph1140_project/format.dart';
+
+abstract class SimulationBaseState<T extends StatefulWidget> extends State<T>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  double get period;
+
+  void resetAnimation() {
+    // For .repeat, Duration only allows int params so use a smaller size
+    //  to keep precision
+    controller.repeat(
+      period: Duration(microseconds: (period * 1000000).toInt()),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this);
+    resetAnimation();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  /// Creates a Slider widget with a label. The $ character is replaced with an
+  ///  appropriate user facing value
+  Widget createSliderValue(String label, double value, double min, double max,
+      Function(double) onChanged) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(
+        width: 200,
+        child: Text(label.replaceAll(r'$', value.format())),
+      ),
+      Expanded(
+        child: Slider(
+          value: value,
+          min: min,
+          max: max,
+          onChanged: (v) {
+            setState(() {
+              onChanged(v);
+              resetAnimation();
+            });
+          },
+        ),
+      ),
+    ]);
+  }
+}
--- lib/spring_simulation.dart
+++ lib/spring_simulation.dart
@@ -2,16 +2,14 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:ph1140_project/format.dart';
+import 'package:ph1140_project/simulation_base.dart';
 
 class SpringWidget extends StatefulWidget {
   @override
   _SpringWidgetState createState() => _SpringWidgetState();
 }
 
-class _SpringWidgetState extends State<SpringWidget>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-
+class _SpringWidgetState extends SimulationBaseState<SpringWidget> {
   double springConstant = 1, mass = 1, amplitude = 1;
 
   double get angularFrequency => sqrt(springConstant / mass);
@@ -21,7 +19,7 @@ class _SpringWidgetState extends State<SpringWidget>
   double get period => 1 / frequency;
 
   double get displacement =>
-      amplitude * cos(angularFrequency * _controller.value * period);
+      amplitude * cos(angularFrequency * controller.value * period);
 
   double get totalEnergy => 0.5 * springConstant * pow(amplitude, 2);
 
@@ -34,33 +32,11 @@ class _SpringWidgetState extends State<SpringWidget>
   double get cubeLength => 50 + 5 * pow(mass, 1 / 3);
 
   @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-    _resetAnimation();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _resetAnimation() {
-    // For .repeat, Duration only allows int params so use a smaller size
-    //  to keep precision
-    _controller.repeat(
-      period: Duration(microseconds: (period * 1000000).toInt()),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Stack(alignment: Alignment.topCenter, children: [
-      // todo equations
       LayoutBuilder(builder: (context, size) {
         return AnimatedBuilder(
-          animation: _controller,
+          animation: controller,
           child: Container(
             width: cubeLength,
             height: cubeLength,
@@ -97,40 +73,15 @@ class _SpringWidgetState extends State<SpringWidget>
           child: Padding(
             padding: EdgeInsets.only(left: 16),
             child: Column(children: [
-              _createSliderValue('Spring Constant (\$ N/m)', springConstant, 1,
+              createSliderValue('Spring Constant (\$ N/m)', springConstant, 1,
                   150, (v) => springConstant = v),
-              _createSliderValue('Mass (\$ kg)', mass, 1, 100, (v) => mass = v),
-              _createSliderValue(
+              createSliderValue('Mass (\$ kg)', mass, 1, 100, (v) => mass = v),
+              createSliderValue(
                   'Amplitude (\$ m)', amplitude, 0.25, 2, (v) => amplitude = v),
             ]),
           ),
         ),
       ]),
-    ]);
-  }
-
-  /// Creates a Slider widget with a label. The $ character is replaced with an
-  ///  appropriate user facing value
-  Widget _createSliderValue(String label, double value, double min, double max,
-      Function(double) onChanged) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Container(
-        width: 200,
-        child: Text(label.replaceAll(r'$', value.format())),
-      ),
-      Expanded(
-        child: Slider(
-          value: value,
-          min: min,
-          max: max,
-          onChanged: (v) {
-            setState(() {
-              onChanged(v);
-              _resetAnimation();
-            });
-          },
-        ),
-      ),
     ]);
   }
 

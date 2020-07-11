db085fec85918c2481f2bc88c7341c3ba8b92558
GregoryConrad <gregorysconrad@gmail.com>
2020-07-11 12:03:15 -0400

    Added state management for play/pause

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ph1140_project/simple_pendulum_simulation.dart';
 import 'package:ph1140_project/spring_simulation.dart';
+import 'package:rxdart/rxdart.dart';
+
+final stateStream = BehaviorSubject.seeded(true);
 
 void main() {
   runApp(MaterialApp(
@@ -10,49 +13,44 @@ void main() {
       brightness: Brightness.dark,
       visualDensity: VisualDensity.adaptivePlatformDensity,
     ),
-    home: MainPage(),
+    home: StreamBuilder<bool>(
+        stream: stateStream,
+        builder: (context, snapshot) {
+          final isLarge = MediaQuery.of(context).size.width > 800;
+          return DefaultTabController(
+            length: 2,
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text('Simple Harmonic Oscillators'),
+                actions: [
+                  if (snapshot.data)
+                    IconButton(
+                        icon: Icon(Icons.pause),
+                        onPressed: () => stateStream.add(false))
+                  else
+                    IconButton(
+                        icon: Icon(Icons.play_arrow),
+                        onPressed: () => stateStream.add(true))
+                ],
+                bottom: isLarge
+                    ? null
+                    : TabBar(tabs: [
+                        Tab(text: 'Spring'),
+                        Tab(text: 'Pendulum'),
+                      ]),
+              ),
+              body: isLarge
+                  ? Row(children: [
+                      Expanded(child: SpringWidget()),
+                      VerticalDivider(thickness: 2),
+                      Expanded(child: SimplePendulumWidget()),
+                    ])
+                  : TabBarView(children: [
+                      SpringWidget(),
+                      SimplePendulumWidget(),
+                    ]),
+            ),
+          );
+        }),
   ));
-}
-
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  bool isPlaying = true;
-
-  @override
-  Widget build(BuildContext context) {
-    final isLarge = MediaQuery.of(context).size.width > 800;
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Simple Harmonic Oscillators'),
-          actions: [
-            // todo use rxdart and behavior subject to take care of this state
-            if (isPlaying)
-              IconButton(
-                  icon: Icon(Icons.pause),
-                  onPressed: () => setState(() => isPlaying = false))
-            else
-              IconButton(
-                  icon: Icon(Icons.play_arrow),
-                  onPressed: () => setState(() => isPlaying = true))
-          ],
-          bottom: isLarge
-              ? null
-              : TabBar(tabs: [Tab(text: 'Spring'), Tab(text: 'Pendulum')]),
-        ),
-        body: isLarge
-            ? Row(children: [
-                Expanded(child: SpringWidget()),
-                VerticalDivider(thickness: 2),
-                Expanded(child: SimplePendulumWidget()),
-              ])
-            : TabBarView(children: [SpringWidget(), SimplePendulumWidget()]),
-      ),
-    );
-  }
 }

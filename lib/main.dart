927b3c330a8b3719bf843bacaa981638956556c6
GregoryConrad <gregorysconrad@gmail.com>
2020-07-10 23:11:17 -0400

    Added base for play/pause

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,49 @@ void main() {
       brightness: Brightness.dark,
       visualDensity: VisualDensity.adaptivePlatformDensity,
     ),
-    home: Builder(builder: (context) {
-      final isLarge = MediaQuery.of(context).size.width > 800;
-      return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Simple Harmonic Oscillators'),
-            actions: [
-              // todo pause/start button
-            ],
-            bottom: isLarge
-                ? null
-                : TabBar(tabs: [Tab(text: 'Spring'), Tab(text: 'Pendulum')]),
-          ),
-          body: isLarge
-              ? Row(children: [
-                  Expanded(child: SpringWidget()),
-                  VerticalDivider(thickness: 2),
-                  Expanded(child: SimplePendulumWidget()),
-                ])
-              : TabBarView(children: [SpringWidget(), SimplePendulumWidget()]),
-        ),
-      );
-    }),
+    home: MainPage(),
   ));
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  bool isPlaying = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final isLarge = MediaQuery.of(context).size.width > 800;
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple Harmonic Oscillators'),
+          actions: [
+            // todo use rxdart and behavior subject to take care of this state
+            if (isPlaying)
+              IconButton(
+                  icon: Icon(Icons.pause),
+                  onPressed: () => setState(() => isPlaying = false))
+            else
+              IconButton(
+                  icon: Icon(Icons.play_arrow),
+                  onPressed: () => setState(() => isPlaying = true))
+          ],
+          bottom: isLarge
+              ? null
+              : TabBar(tabs: [Tab(text: 'Spring'), Tab(text: 'Pendulum')]),
+        ),
+        body: isLarge
+            ? Row(children: [
+                Expanded(child: SpringWidget()),
+                VerticalDivider(thickness: 2),
+                Expanded(child: SimplePendulumWidget()),
+              ])
+            : TabBarView(children: [SpringWidget(), SimplePendulumWidget()]),
+      ),
+    );
+  }
 }
